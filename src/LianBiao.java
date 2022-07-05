843fa2e7647033e0d7f8b4f3f91e84bdacce2fc9
Leon Zhang <leonz@synnex.com>
2022-07-05 15:51:58 +0800

    Update LianBiao.java
    单链表队列

--- src/LianBiao.java
+++ src/LianBiao.java
@@ -1,22 +1,79 @@
 public class LianBiao {
 
-    public static class Node {
-        public int a;
-        public Node next;
+    public static class SingNode<V> {
+        public V value;
+        public SingNode next;
 
-
-        public Node(int a) {
-            this.a = a;
+        public SingNode(V value) {
+            this.value = value;
+            this.next = null;
         }
 
-        public Node() {
+        public SingNode() {
         }
+    }
+
+    public static class MySingQueue<V> {
+        private SingNode<V> head;
+        private SingNode<V> tail;
+        private int size;
+
+        public MySingQueue(SingNode singNode) {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public MySingQueue() {
+
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void add(V value) {
+            SingNode<V> vSingNode = new SingNode<>(value);
+            if (tail == null) {
+                head = vSingNode;
+            } else {
+                tail.next = vSingNode;
+            }
+            tail = vSingNode;
+            size++;
+
+        }
+
+        public V poll() {
+            V ans = null;
+
+            if (head != null) {
+                ans = head.value;
+                head = head.next;
+                size--;
+            } else {
+                tail = null;
+            }
+            return ans;
+        }
+
     }
 
 
     public static void main(String[] args) {
-        Node a = new Node();
-        System.out.println(a.a);
-        a.next = new Node();
+
+
+        MySingQueue mySingQueue = new MySingQueue();
+        mySingQueue.add(1);
+        mySingQueue.add("dsadsa");
+        mySingQueue.add("dsadsaz22");
+        System.out.println(mySingQueue.poll());
+        System.out.println(mySingQueue.poll());
+        System.out.println(mySingQueue.poll());
+
     }
 }
