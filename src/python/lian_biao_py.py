97be7f1af3f6b15b31223ad85e8fcc227af6987a
Leon Zhang <leonz@synnex.com>
2022-07-08 10:54:35 +0800

    123456

--- src/python/lian_biao_py.py
+++ src/python/lian_biao_py.py
@@ -1,66 +1,73 @@
-class SingQueue:
-
+class fuxu_lianbiao:
     def __init__(self, value):
         self.value = value
         self.next = None
 
 
-class SingleList:
+class singer_list:
     def __init__(self):
-        self.head = None
         self.count = 0
+        self.head = None
+        self.tail = None
 
-    def is_empty(self):
+    def is_emptye(self):
         return self.count == 0
 
     def length(self):
         return self.count
 
     def add(self, item):
-        new = SingQueue(item)
-        if self.is_empty():
+        new = fuxu_lianbiao(item)
+        if self.tail is None:
             self.head = new
         else:
-            new.next = self.head
-            self.head = new
+            self.tail.next = new
+        self.tail = new
+
         self.count += 1
 
-    def get_all(self):
-        ans = self.head
-        while ans is not None:
-            print(ans.value)
-            ans = ans.next
-
     def poll(self):
-        ans = None
+        cur = None
         if self.head is None:
-            return ans
+            return cur
         else:
-            ans = self.head.value
+            cur = self.head.value
             self.head = self.head.next
             self.count -= 1
-        return ans
+        return cur
+
+    def get_all(self):
+        cur = self.head
+        while cur is not None:
+            print(cur.value)
+            cur = cur.next
+
+
+class singer_stack:
+    def __init__(self):
+        self.head = None
+        self.count = 0
+
+    def add(self, one):
+        ac = fuxu_lianbiao(one)
+        if self.head is None:
+            self.head = ac
+        else:
+            ac.next = self.head
+            self.head = ac
+
+    def get_all(self):
+        ans = None
+        while self.head is not None:
+            ans = self.head.value
+            print(ans)
+            self.head = self.head.next
 
 
 if __name__ == '__main__':
-    a = SingleList()
-    print(a.length())
-    a.add(1)
-    a.add('dda')
-    a.add('ddacc')
-    print(a.length())
+    a = singer_stack()
+    a.add('a')
+    a.add('b')
+    a.add('c')
+    a.add('d')
     a.get_all()
-    print('-------------')
-    print(a.poll())
-    print(a.length())
-    print(a.poll())
-    print(a.length())
-    print(a.poll())
-    print(a.length())
-    print('--------------------')
-    print(a.poll())
-    print(a.length())
-    print('--------------------')
-    print(a.poll())
-    print(a.length())
-    print(a.get_all())
