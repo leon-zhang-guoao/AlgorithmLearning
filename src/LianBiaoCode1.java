c5586e2d9c58d18762f81cb9de52f944dc18b49e
leon <52643518+leon-zhang-guoao@users.noreply.github.com>
2022-07-12 20:51:13 +0800

    123

--- src/LianBiaoCode1.java
+++ src/LianBiaoCode1.java
@@ -1,26 +1,54 @@
 public class LianBiaoCode1 {
 
     //给一个 k 值 要求一个链表在 k值为一组的 范围内 翻转 不满k值一组的 不操作
-    public class Node {
+    // https://leetcode.cn/problems/reverse-nodes-in-k-group/
+    public class ListNode {
         public int value;
-        public Node next;
+        public ListNode next;
     }
 
-    public static Node getlastFromK(Node n, int k) {
-        while (k > 0) {
-            if (n != null) {
-                n = n.next;
+
+    public static ListNode fin(ListNode head, int k) {
+        ListNode start = head;
+        ListNode end = getlastFromK(head, k);
+        if (end == null) {
+
+            return head;
+        }
+        head = end;
+        fanZhuan(start, end);
+        ListNode lastEnd = start;
+        while (lastEnd.next != null) {
+            start = lastEnd.next;
+            end = getlastFromK(start, k);
+            if (end == null) {
+                return head;
             }
-            k--;
+            fanZhuan(start, end);
+            lastEnd.next = end;
+            lastEnd = start;
+
+
+        }
+
+        return head;
+    }
+
+    public static ListNode getlastFromK(ListNode n, int k) {
+        while (--k != 0 && n != null) {
+
+            n = n.next;
+
+
         }
         return n;
     }
 
-    public static void fanZhuan(Node head, Node last) {
+    public static void fanZhuan(ListNode head, ListNode last) {
         last = last.next;
-        Node pre = null;
-        Node cur = head;
-        Node next = null;
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
         while (cur != last) {
 
             next = cur.next;
@@ -37,9 +65,11 @@ public class LianBiaoCode1 {
 
     public static void main(String[] args) {
         int a = 5;
-        while (a >= 0) {
-            System.out.println(a);
-            a--;
+        int b =6 ;
+        while (a-- != 0 && b != 0) {
+            System.out.println(b);
+            b--;
+
         }
 
     }
---
+++ src/python/lian_biao_code1.py
@@ -0,0 +1,11 @@
+class LianBiaoCode:
+    def __init__(self, i):
+        self.i = i
+        self.next = None
+
+
+class Solution:
+    def reverseKGroup(self, head, k):
+        return head
+
+    
