e520b4cf921e339013ea36d2f2d67591a744cb70
Leon Zhang <leonz@synnex.com>
2022-07-18 15:32:08 +0800

    456

--- src/LianBiaopg/LianBiaoCode4.java
+++ src/LianBiaopg/LianBiaoCode4.java
@@ -26,16 +26,26 @@ public class LianBiaoCode4 {
         // one 是 开头 小的
         ListNode one = a >= b ? list2 : list1;
         ListNode two = a <= b ? list2 : list1;
-        while (one != null && two != null){
+        ListNode one_cur = one;
+        ListNode two_cur = two;
+        one_cur.next = two_cur;
+        one_cur = one_cur.next;
+
+        while (one_cur != null && two_cur != null) {
+            if (one_cur.val <= two_cur.next.val){
+                two_cur.next = one_cur;
+                two_cur = two_cur.next;
+            } else {
+                two_cur = two_cur.next;
+            }
 
 
         }
 
 
-        return list1;
+        return one;
 
     }
-
 
 
 }
