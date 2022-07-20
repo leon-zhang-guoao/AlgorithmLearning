150ef0b097d06bfb866d660f47bfefc70e7fa783
leon <52643518+leon-zhang-guoao@users.noreply.github.com>
2022-07-20 22:55:30 +0800

    1231

--- src/bitMap/ADD.java
+++ src/bitMap/ADD.java
@@ -2,6 +2,7 @@ package bitMap;
 
 //用二进制实现加法
 public class ADD {
+    //加法
     public static int add1(int a, int b) {
         int sum = 0;
         while (b != 0) {
@@ -13,8 +14,33 @@ public class ADD {
 
     }
 
+    //负数
+    public static int negNum(int a) {
+        return add1(a, 1);
+    }
+
+    //减法
+    public static int neg(int a, int b) {
+
+        return add1(a, negNum(b));
+
+
+    }
+
+    public static int mulite(int a, int b) {
+        int res = 0;
+        while (b != 0) {
+            if ((b & 1) != 0) {
+                res = add1(res,a);
+            }
+            a <<= 1;
+            b >>>= 1;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        int i = add1(11, 33);
+        int i = mulite(3, 7);
         System.out.println(i);
 
     }
